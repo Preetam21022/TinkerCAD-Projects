495b1b6a08d02303a91168353714aa29dcba48f5
Preetam Kumar Ghosh <ghosh2303066@stud.kuet.ac.bd>
2025-07-02 04:08:30 +0600

    Update sketch_jul2a.ino

--- Interfacing Photodiode/sketch_jul2a/sketch_jul2a.ino
+++ Interfacing Photodiode/sketch_jul2a/sketch_jul2a.ino
@@ -9,7 +9,7 @@ void setup() {
 void loop() {
   val = analogRead(pd); // Read analog voltage value (scaled from 0 to 1023)
   // Convert analog value to voltage (0–5V range)
-  float voltage = (val / 1023.0) * 5.0;
+  float voltage = (val / 85) * 5.0;
 
   Serial.print("Analog Value: ");
   Serial.println(val);
@@ -18,13 +18,4 @@ void loop() {
   Serial.print(voltage);
   Serial.println(" V");
 }
-/*
-int val = analogRead(pd);  // analogRead gives 0 to 1023
-float voltage = map(val, 0, 1023, 0, 500) / 100.0; // convert to 0.00–5.00V
 //***map fuction gives intiger value as output***
-Serial.print("Analog Value: ");
-Serial.println(val);
-Serial.print("Voltage: ");
-Serial.print(voltage);
-Serial.println(" V");
-*/
