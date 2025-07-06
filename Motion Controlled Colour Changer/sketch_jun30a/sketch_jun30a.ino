8b32eea5e0dc996c64a20ea3a29902a7ac063488
Preetam Kumar Ghosh <ghosh2303066@stud.kuet.ac.bd>
2025-07-06 18:24:33 +0600

    Update sketch_jun30a.ino

--- Motion Controlled Colour Changer/sketch_jun30a/sketch_jun30a.ino
+++ Motion Controlled Colour Changer/sketch_jun30a/sketch_jun30a.ino
@@ -3,16 +3,24 @@ int pir= 7;
 int red= 2;
 int blue= 3;
 int green= 4;
+int ldrPin = A0;
+int threshold = 500;  // Adjust this based on ambient light
+
 
 void setup() {
   pinMode(pir, INPUT);
+  digitalWrite(pir, LOW); // Keep PIR OFF initially
   pinMode(buzzer, OUTPUT);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  int lightLevel = analogRead(ldrPin);   // Read LDR value
+  Serial.println(lightLevel);
+  if(lightLevel<=threshold) {
   if(digitalRead(pir) == HIGH) {
     digitalWrite(buzzer, HIGH);
     RBG_color(0,255,0);//blue
@@ -26,6 +34,9 @@ void loop() {
     //as we use RCBG instead of RCGB LED, we have to write RBG_color = (255,0,255);
     //if we used the right sequence RCGB LED, we would write RGB_color = (255,255,0); as shown in the video
   }
+  }
+  else
+    digitalRead(pir) == LOW;
 }
 
 void RBG_color(int red_value, int blue_value, int green_value) {
